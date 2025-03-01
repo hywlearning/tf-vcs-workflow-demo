acface5bb12eeb51265f3973104d7018f85fe63d
hywlearning <hninyuwai.learning@gmail.com>
2025-03-01 14:28:36 +0800

    destroy vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_vpc" "hello_vpc" {
 
   cidr_block           = "192.168.0.0/16"
@@ -11,3 +12,4 @@ resource "aws_vpc" "hello_vpc_2" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
+*/

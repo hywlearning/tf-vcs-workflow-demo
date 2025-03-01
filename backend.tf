87d9dc64a2c9842f605281220da16f99e83df839
hywlearning <hninyuwai.learning@gmail.com>
2025-03-01 14:48:01 +0800

    added backend file

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  cloud {
+    organization = "terraform-hyw"
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for HCP Terraform
+    hostname = "app.terraform.io"
+
+    workspaces {
+      name = "tf-vcs-workflow-demo"
+      # name = "tf-vcs-workflow-planonly"
+    }
+  }
+}
